61d28d7032009a506e158720918383e278ed2d74
John Jairo Castro <168293591+johnjacama24@users.noreply.github.com>
2025-07-07 18:49:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ if st.button("🔍 Realizar predicción"):
 
         pred_codificada = modelo.predict(entrada)[0]
         st.write("🔢 Código predicho:", pred_codificada)
-st.write("🔑 Claves diccionario deserción:", dicc_deserción.keys())
-st.write("🧪 Tipo:", type(pred_codificada))
+        st.write("🔑 Claves diccionario deserción:", dicc_deserción.keys())
+        st.write("🧪 Tipo:", type(pred_codificada))
         pred_original = dicc_deserción.get(pred_codificada, "Desconocido")
 
         st.success(f"✅ Estado del aprendiz predicho: **{pred_original}**")
